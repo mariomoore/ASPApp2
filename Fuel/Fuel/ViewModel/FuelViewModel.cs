2757dcdc606b7760a026eec01b0b08f60d07b8f2
Mariusz Murawski <mariusz.murawski84@gmail.com>
2017-11-18 09:47:57 +0100

    Dodanie Modeli

---
+++ Fuel/Fuel/ViewModel/FuelViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Fuel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Fuel.ViewModel
+{
+    public class FuelViewModel
+    {
+        public FuelModel FuelModel { get; set; }
+        public bool ShowResult { get; set; }
+    }
+}
